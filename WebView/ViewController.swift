b106dba048d2ee038671bbf29bbbca8a959e4f0a
Rinchin <gomboev.rinchin@gmail.com>
2016-07-09 22:37:32 +0300

    Initial Commit

---
+++ WebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by RInchin on 09.07.16.
+//  Copyright © 2016 RInchin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
